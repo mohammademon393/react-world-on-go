e8730898eac4729a2ec6fd369f6590faa25efa87
mohammademon393 <mdemon357ep@gmail.com>
2025-09-18 22:58:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Countries from './component/countries/Countries'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>React world no go...</h1>
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/component/countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>World tour is redy......</h1>
+        </div>
+    );
+};
+
+export default Countries;
