0a098e07ae0396ea9fe1f8ecc18084e999117366
mohammademon393 <mdemon357ep@gmail.com>
2025-09-18 23:30:42 +0600

    create a component and props the countries value

---
+++ src/component/Country/Country.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Country = ({country}) => {
+
+    console.log(country);
+    
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Country;
--- src/component/countries/Countries.jsx
+++ src/component/countries/Countries.jsx
@@ -1,13 +1,17 @@
-import React, { use } from 'react';
+import React, { use } from "react";
+import Country from "../Country/Country";
 
 const Countries = ({ countriesPromies }) => {
-    const countriesData = use(countriesPromies);
-    const countries = countriesData.countries;
-    console.log(countries);
-    
+  const countriesData = use(countriesPromies);
+  const countries = countriesData.countries;
+  console.log(countries);
+
   return (
     <div>
       <h1>React on the goooo: {countries.length} </h1>
+      {
+        countries.map(country => <Country country={country}></Country>)
+      }
     </div>
   );
 };
